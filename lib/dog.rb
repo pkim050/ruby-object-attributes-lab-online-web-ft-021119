3ee0694faafbdd68ce3d8544c3a0278dd83b11ab
Patrick Kim <pkim050@gmail.com>
2019-02-18 23:25:01 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed
+    @breed
+  end
+  
+  def breed=(breeds)
+    @breed = breeds
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name
+    @name
+  end
+  
+  def name=(persons_name)
+    @name = persons_name
+  end
+  
+  def job
+    @job
+  end
+  
+  def job=(title)
+    @job = title
+  end
+end
